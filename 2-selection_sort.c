44c5e0910f146b64f2f456e95d1d42b386f37c0b
asamoah953 <asamoahclinton953@gmail.com>
2024-04-30 09:18:41 -0700

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * selection_sort -This sorts array of ints using the selction sort
+ * algorithm
+ * @array: array of ints to be sorted
+ * @size: amount of elements the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int p, m, min_j, temp, n = (int)size;
+
+	if (!array || size < 2)
+		return;
+
+	/* at every position in the n-member array */
+	for (p = 0; p < n - 1; p++)
+	{
+		/* scan from that position to the end, */
+		min_j = i;
+		for (m = p + 1; m < n; m++)
+		{
+			/* determine the minimum value in that range */
+			if (array[j] < array[min_j])
+			{
+				min_j = m;
+			}
+		}
+		/* if lower than the value at start of range, */
+		/* then swap them */
+		if (min_j != p)
+		{
+			temp = array[p];
+			array[p] = array[min_j];
+			array[min_j] = temp;
+			print_array(array, size);
+		}
+	}
+}
